f7601829ac53122c5d9a81ec3fd53a6c33697ccf
Md Asif Alam <mdasifalam910@gmail.com>
2023-10-04 15:38:40 +0530

    useState with Ternary

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { useState } from "react";
 
 const App = () => {
-  let [myName, setMyName] = useState("Thapa Technical");
+  const [myName, setMyName] = useState("Thapa Technical");
 
   const updateName = () => {
-    if (myName === "Thapa Technical")
-      setMyName("Thanks for subscribing the channel");
-    else setMyName("Thapa Technical");
+    // if (myName === "Thapa Technical")
+    //   setMyName("Thanks for subscribing the channel");
+    // else setMyName("Thapa Technical");
+
+    myName === "Thapa Technical"
+      ? setMyName("Thanks for subscribing the channel")
+      : setMyName("Thapa Technical");
   };
 
   return (
